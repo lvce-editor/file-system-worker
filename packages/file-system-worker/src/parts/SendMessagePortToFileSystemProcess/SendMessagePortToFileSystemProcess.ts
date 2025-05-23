313612b23627a39fff582e7f0b090a7c34cdb12c
Le Vivilet <levivlet@pm.me>
2025-05-24 00:08:58 +0200

    feature: rename renderer worker file

--- packages/file-system-worker/src/parts/SendMessagePortToFileSystemProcess/SendMessagePortToFileSystemProcess.ts
+++ packages/file-system-worker/src/parts/SendMessagePortToFileSystemProcess/SendMessagePortToFileSystemProcess.ts
@@ -1,4 +1,4 @@
-import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
+import * as ParentRpc from '../RendererWorker/RendererWorker.ts'
 import * as RpcId from '../RpcId/RpcId.ts'
 
 export const sendMessagePortToFileSystemProcess = async (port: MessagePort): Promise<void> => {
