39e4a76fbbc58c6b3e459b550f0790ddccf2b0df
Le Vivilet <levivlet@pm.me>
2025-05-24 00:47:19 +0200

    feature: add function to launch websocket rpc

--- packages/file-system-worker/src/parts/CreateFileSystemProcessRpcNode/CreateFileSystemProcessRpcNode.ts
+++ packages/file-system-worker/src/parts/CreateFileSystemProcessRpcNode/CreateFileSystemProcessRpcNode.ts
@@ -1,10 +1,19 @@
-import { type Rpc } from '@lvce-editor/rpc'
+import { type Rpc, WebSocketRpcParent } from '@lvce-editor/rpc'
 import { VError } from '@lvce-editor/verror'
+import * as GetWebSocketUrl from '../GetWebSocketUrl/GetWebSocketUrl.ts'
+import * as Location from '../Location/Location.ts'
 
 export const createFileSystemProcessRpcNode = async (): Promise<Rpc> => {
   try {
-    // TODO create websocket rpc
-    return {} as any
+    const host = Location.getHost()
+    const protocol = Location.getProtocol()
+    const wsUrl = GetWebSocketUrl.getWebSocketUrl('file-system-process', host, protocol)
+    const webSocket = new WebSocket(wsUrl)
+    const rpc = await WebSocketRpcParent.create({
+      webSocket,
+      commandMap: {},
+    })
+    return rpc
   } catch (error) {
     throw new VError(error, `Failed to create file system process rpc`)
   }
