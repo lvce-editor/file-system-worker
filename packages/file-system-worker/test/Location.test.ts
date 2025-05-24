89575464b4979deb37c71021453de9813c6b2db7
Le Vivilet <levivlet@pm.me>
2025-05-24 14:34:36 +0200

    test

--- packages/file-system-worker/test/Location.test.ts
+++ packages/file-system-worker/test/Location.test.ts
@@ -2,31 +2,17 @@ import { test, expect } from '@jest/globals'
 import { getHost, getProtocol } from '../src/parts/Location/Location.js'
 
 test('getHost returns location.host', () => {
-  const originalHost = globalThis.location.host
-  Object.defineProperty(globalThis, 'location', {
-    value: { host: 'test.example.com' },
-    writable: true,
-  })
-
+  // @ts-ignore
+  globalThis.location = {
+    host: 'test.example.com',
+  }
   expect(getHost()).toBe('test.example.com')
-
-  Object.defineProperty(globalThis, 'location', {
-    value: { host: originalHost },
-    writable: true,
-  })
 })
 
 test('getProtocol returns location.protocol', () => {
-  const originalProtocol = globalThis.location.protocol
-  Object.defineProperty(globalThis, 'location', {
-    value: { protocol: 'https:' },
-    writable: true,
-  })
-
+  // @ts-ignore
+  globalThis.location = {
+    protocol: 'https:',
+  }
   expect(getProtocol()).toBe('https:')
-
-  Object.defineProperty(globalThis, 'location', {
-    value: { protocol: originalProtocol },
-    writable: true,
-  })
 })
