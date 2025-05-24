9c8ced7d1218864561615b348241a05153f33241
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-24 14:24:25 +0200

    feature: add electron rpc test (#21)
    * feature: add electron rpc test
    
    * lint

---
+++ packages/file-system-worker/test/CreateFileSystemProcessRpcElectron.test.ts
@@ -0,0 +1,37 @@
+import { expect, jest, test } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
+import { createFileSystemProcessRpcElectron } from '../src/parts/CreateFileSystemProcessRpcElectron/CreateFileSystemProcessRpcElectron.js'
+import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.js'
+
+test('creates file system process rpc', async () => {
+  const mockInvokeAndTransfer = jest.fn()
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: () => Promise.resolve(),
+    invokeAndTransfer: mockInvokeAndTransfer,
+  })
+  RendererWorker.set(mockRpc)
+  const rpc = await createFileSystemProcessRpcElectron()
+  expect(mockInvokeAndTransfer).toHaveBeenCalledTimes(1)
+  expect(mockInvokeAndTransfer).toHaveBeenCalledWith(
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToSharedProcess',
+    expect.anything(),
+    'HandleMessagePortForFileSystemProcess.handleMessagePortForFileSystemProcess',
+    209,
+  )
+  expect(rpc).toBeDefined()
+  await rpc.dispose()
+})
+
+test('handles error when creating file system process rpc', async () => {
+  const mockInvokeAndTransfer = jest.fn(() => {
+    throw new Error('test error')
+  })
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: () => Promise.resolve(),
+    invokeAndTransfer: mockInvokeAndTransfer,
+  })
+  RendererWorker.set(mockRpc)
+  await expect(createFileSystemProcessRpcElectron()).rejects.toThrow('Failed to create file system process rpc')
+})
