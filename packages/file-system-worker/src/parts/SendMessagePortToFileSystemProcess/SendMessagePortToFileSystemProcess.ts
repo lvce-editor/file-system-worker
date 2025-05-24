68a28462e0a914e59d056558f865a1f1fb712f87
Le Vivilet <levivlet@pm.me>
2025-05-24 14:09:20 +0200

    feature: simplify rpc code

--- packages/file-system-worker/src/parts/SendMessagePortToFileSystemProcess/SendMessagePortToFileSystemProcess.ts
+++ packages/file-system-worker/src/parts/SendMessagePortToFileSystemProcess/SendMessagePortToFileSystemProcess.ts
@@ -1,5 +1,5 @@
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
-import * as RpcId from '../RpcId/RpcId.ts'
+import { RpcId } from '@lvce-editor/rpc-registry'
 
 export const sendMessagePortToFileSystemProcess = async (port: MessagePort): Promise<void> => {
   const command = 'HandleMessagePortForFileSystemProcess.handleMessagePortForFileSystemProcess'
