ab949ae5127086c4e863920615a6be3aaaf50bcb
Le Vivilet <levivlet@pm.me>
2025-05-24 14:12:12 +0200

    lint

--- packages/file-system-worker/src/parts/SendMessagePortToFileSystemProcess/SendMessagePortToFileSystemProcess.ts
+++ packages/file-system-worker/src/parts/SendMessagePortToFileSystemProcess/SendMessagePortToFileSystemProcess.ts
@@ -1,5 +1,5 @@
-import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 import { RpcId } from '@lvce-editor/rpc-registry'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const sendMessagePortToFileSystemProcess = async (port: MessagePort): Promise<void> => {
   const command = 'HandleMessagePortForFileSystemProcess.handleMessagePortForFileSystemProcess'
