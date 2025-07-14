a7d0b888eb41e341b1301f2c764d073bb97c7adb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-14 14:28:02 +0200

    feature: add fs exists (#53)
    * feature: add fs exists
    
    * memor

--- packages/file-system-worker/src/parts/FileSystemProcess/FileSystemProcess.ts
+++ packages/file-system-worker/src/parts/FileSystemProcess/FileSystemProcess.ts
@@ -1,4 +1,5 @@
 import { FileSystemProcess } from '@lvce-editor/rpc-registry'
 
-export const { set, rename, copy, mkdir, getFolderSize, writeFile, stat, readJson, getPathSeparator, getRealPath, readDirWithFileTypes, readFile, remove } =
+// @ts-ignore
+export const { set, rename, copy, mkdir, getFolderSize, writeFile, stat, readJson, getPathSeparator, getRealPath, readDirWithFileTypes, readFile, remove, exists } =
   FileSystemProcess
