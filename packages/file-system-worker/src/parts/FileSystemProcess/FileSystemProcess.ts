5c2557718f7dd5828ef133d28ffd55a989d86e98
Le Vivilet <levivlet@pm.me>
2025-07-17 22:09:55 +0200

    feature: remove ts ignore

--- packages/file-system-worker/src/parts/FileSystemProcess/FileSystemProcess.ts
+++ packages/file-system-worker/src/parts/FileSystemProcess/FileSystemProcess.ts
@@ -1,5 +1,4 @@
 import { FileSystemProcess } from '@lvce-editor/rpc-registry'
 
-// @ts-ignore
 export const { set, rename, copy, mkdir, getFolderSize, writeFile, stat, readJson, getPathSeparator, getRealPath, readDirWithFileTypes, readFile, remove, exists } =
   FileSystemProcess
