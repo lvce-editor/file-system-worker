7f43908c07940516656a1d87bb5c8c86dbacf37a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-14 12:12:00 +0200

    feature: register watchFile command (#73)

--- packages/file-system-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/file-system-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,24 +1,27 @@
 import * as FileSystem from '../FileSystemDisk/FileSystemDisk.ts'
+import * as FileWatcher from '../FileWatcher/FileWatcher.ts'
 import * as HandleMessagePort from '../HandleMessagePort/HandleMessagePort.ts'
 import * as Initialize from '../Initialize/Initialize.ts'
 
 export const commandMap: Record<string, any> = {
+  'FileSystem.appendFile': FileSystem.appendFile,
   'FileSystem.copy': FileSystem.copy,
   'FileSystem.createFile': FileSystem.createFile,
   'FileSystem.exists': FileSystem.exists,
   'FileSystem.getFolderSize': FileSystem.getFolderSize,
   'FileSystem.getPathSeparator': FileSystem.getPathSeparator,
   'FileSystem.getRealPath': FileSystem.getRealPath,
+  'FileSystem.handleMessagePort': HandleMessagePort.handleMessagePort,
   'FileSystem.mkdir': FileSystem.mkdir,
   'FileSystem.readDirWithFileTypes': FileSystem.readDirWithFileTypes,
   'FileSystem.readFile': FileSystem.readFile,
+  'FileSystem.readFileAsBlob': FileSystem.readFileAsBlob,
   'FileSystem.readJson': FileSystem.readJson,
   'FileSystem.remove': FileSystem.remove,
   'FileSystem.rename': FileSystem.rename,
   'FileSystem.stat': FileSystem.stat,
+  'FileSystem.watchFile': FileWatcher.watchFile,
+  'FileSystem.unwatchFile': FileWatcher.unwatchFile,
   'FileSystem.writeFile': FileSystem.writeFile,
-  'FileSystem.appendFile': FileSystem.appendFile,
-  'FileSystem.handleMessagePort': HandleMessagePort.handleMessagePort,
-  'FileSystem.readFileAsBlob': FileSystem.readFileAsBlob,
   'Initialize.initialize': Initialize.initialize,
 }
