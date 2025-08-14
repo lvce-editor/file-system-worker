b6a603d86fc7cf764685cccb6863b0ded100547a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-14 09:17:52 +0200

    feature: register message port if rpc is set (#70)
    * feature: register message port if rpc is set
    
    * coverage

--- packages/file-system-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
+++ packages/file-system-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
@@ -1,8 +1,12 @@
 import { PlainMessagePortRpc } from '@lvce-editor/rpc'
+import { set } from '@lvce-editor/rpc-registry'
 
 export const handleMessagePort = async (port: MessagePort, rpcId?: number): Promise<void> => {
-  await PlainMessagePortRpc.create({
+  const rpc = await PlainMessagePortRpc.create({
     commandMap: {},
     messagePort: port,
   })
+  if (rpcId) {
+    set(rpcId, rpc)
+  }
 }
