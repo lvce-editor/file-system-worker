461739795f0a90ef9316489c45ccb92c669b252a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-14 09:23:14 +0200

    feature: add file watcher functions (#71)
    * feature: add file watcher functions
    
    * lint
    
    * lint

--- packages/file-system-worker/src/parts/FileSystemProcess/FileSystemProcess.ts
+++ packages/file-system-worker/src/parts/FileSystemProcess/FileSystemProcess.ts
@@ -1,4 +1,19 @@
 import { FileSystemProcess } from '@lvce-editor/rpc-registry'
 
-export const { set, rename, copy, mkdir, getFolderSize, writeFile, stat, readJson, getPathSeparator, getRealPath, readDirWithFileTypes, readFile, remove, exists } =
-  FileSystemProcess
+export const {
+  set,
+  rename,
+  copy,
+  mkdir,
+  invoke,
+  getFolderSize,
+  writeFile,
+  stat,
+  readJson,
+  getPathSeparator,
+  getRealPath,
+  readDirWithFileTypes,
+  readFile,
+  remove,
+  exists,
+} = FileSystemProcess
---
+++ packages/file-system-worker/src/parts/FileWatcher/FileWatcher.ts
@@ -0,0 +1,22 @@
+import type { Rpc } from '@lvce-editor/rpc'
+import * as FileSystemProcess from '../FileSystemProcess/FileSystemProcess.ts'
+import * as WatchCallbacks from '../WatchCallbacks/WatchCallbacks.ts'
+
+export const watchFile = async (id: number, uri: string, rpc: Rpc): Promise<void> => {
+  const commandId = 'Output.executeWatchCallback'
+  WatchCallbacks.registerWatchCallback(id, async () => {
+    await rpc.invoke(commandId)
+  })
+  // @ts-ignore
+  await FileSystemProcess.invoke('FileSystem.watchFile', id, uri)
+}
+
+export const fire = async (id: number): Promise<void> => {
+  await WatchCallbacks.executeWatchCallBack(id)
+}
+
+export const unwatchFile = async (id: number): Promise<void> => {
+  WatchCallbacks.unregisterWatchCallback(id)
+  // @ts-ignore
+  await FileSystemProcess.invoke('FileSystem.unwatchFile', id)
+}
