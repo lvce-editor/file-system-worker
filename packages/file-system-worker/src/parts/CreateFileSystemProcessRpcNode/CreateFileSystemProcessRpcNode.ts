069e726218a84761b02e093496090f8e105948ff
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-15 11:51:05 +0200

    feature: make commandmap available to file system process (#76)

--- packages/file-system-worker/src/parts/CreateFileSystemProcessRpcNode/CreateFileSystemProcessRpcNode.ts
+++ packages/file-system-worker/src/parts/CreateFileSystemProcessRpcNode/CreateFileSystemProcessRpcNode.ts
@@ -1,11 +1,12 @@
 import { type Rpc, WebSocketRpcParent2 } from '@lvce-editor/rpc'
 import { VError } from '@lvce-editor/verror'
+import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
 
 export const createFileSystemProcessRpcNode = async (): Promise<Rpc> => {
   try {
     const rpc = await WebSocketRpcParent2.create({
       type: 'file-system-process',
-      commandMap: {},
+      commandMap: CommandMapRef.commandMapRef,
     })
     return rpc
   } catch (error) {
