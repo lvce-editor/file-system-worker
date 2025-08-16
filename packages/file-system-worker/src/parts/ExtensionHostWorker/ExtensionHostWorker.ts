9486dd7d9e247502c8df3ca165ad8709917e531a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-16 22:22:39 +0200

    feature: add fileSystemMemory rpc (#85)

--- packages/file-system-worker/src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
+++ packages/file-system-worker/src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
@@ -1,3 +1,20 @@
+import type { Rpc } from '@lvce-editor/rpc-registry';
 import { ExtensionHost } from '@lvce-editor/rpc-registry'
+import { createExtensionHostRpc } from '../InitializeExtensionHostWorker/InitializeExtensionHostWorker.ts'
 
-export const { set, invoke } = ExtensionHost
+export const { set } = ExtensionHost
+
+let rpcPromise: Promise<Rpc> | undefined = undefined
+
+const getOrCreate = (): Promise<Rpc> => {
+  if (!rpcPromise) {
+    rpcPromise = createExtensionHostRpc()
+  }
+
+  return rpcPromise
+}
+
+export const invoke = async (method: string, ...params: readonly any[]): Promise<any> => {
+  const rpc = await getOrCreate()
+  return rpc.invoke(method, ...params)
+}
