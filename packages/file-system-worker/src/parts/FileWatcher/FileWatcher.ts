ed383dcc552eadfa3dc47cce06decb4e72f331a3
Le Vivilet <levivilet@pm.me>
2025-09-08 21:30:04 +0200

    feature: store watch callback ids instead of closures (#96)
    * feature: store watch callback ids instead of closures
    
    * lint

--- packages/file-system-worker/src/parts/FileWatcher/FileWatcher.ts
+++ packages/file-system-worker/src/parts/FileWatcher/FileWatcher.ts
@@ -1,20 +1,19 @@
-import * as RpcRegistry from '@lvce-editor/rpc-registry'
 import { assertUri } from '../AssertUri/AssertUri.ts'
 import * as FileSystemProcess from '../FileSystemProcess/FileSystemProcess.ts'
+import * as Protocol from '../Protocol/Protocol.ts'
 import * as WatchCallbacks from '../WatchCallbacks/WatchCallbacks.ts'
+
+// TODO support file watchers for differenr protocols like memfs, http
 
 export const watchFile = async (id: number, uri: string, rpcId: number): Promise<void> => {
   assertUri(uri)
   const commandId = 'Output.executeWatchCallback'
-  WatchCallbacks.registerWatchCallback(id, async () => {
-    const rpc = RpcRegistry.get(rpcId)
-    if (!rpc) {
-      return
-    }
-    await rpc.invoke(commandId, id)
-  })
-  // @ts-ignore
-  await FileSystemProcess.invoke('FileSystem.watchFile', id, uri)
+  WatchCallbacks.registerWatchCallback(id, rpcId, commandId)
+
+  if (uri.startsWith(Protocol.File)) {
+    // @ts-ignore
+    await FileSystemProcess.invoke('FileSystem.watchFile', id, uri)
+  }
 }
 
 export const executeWatchCallback = async (id: number): Promise<void> => {
@@ -27,6 +26,7 @@ export const executeWatchCallback = async (id: number): Promise<void> => {
 
 export const unwatchFile = async (id: number): Promise<void> => {
   WatchCallbacks.unregisterWatchCallback(id)
+  // TODO only if it is a file uri
   // @ts-ignore
   await FileSystemProcess.invoke('FileSystem.unwatchFile', id)
 }
