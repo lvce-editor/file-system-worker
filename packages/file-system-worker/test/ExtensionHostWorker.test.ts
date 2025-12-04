12e54e921f1c185ea2f3e2a8b4d4e9fc67b3ff9a
Le Vivilet <levivilet@pm.me>
2025-12-04 17:51:44 +0100

    test

--- packages/file-system-worker/test/ExtensionHostWorker.test.ts
+++ packages/file-system-worker/test/ExtensionHostWorker.test.ts
@@ -1,6 +1,5 @@
-import { test, expect, jest, beforeEach } from '@jest/globals'
+import { beforeEach, expect, jest, test } from '@jest/globals'
 import { MockRpc } from '@lvce-editor/rpc'
-import { ExtensionHost } from '@lvce-editor/rpc-registry'
 import * as ExtensionHostWorker from '../src/parts/ExtensionHostWorker/ExtensionHostWorker.ts'
 
 const mockCreateExtensionHostRpc = jest.fn<() => Promise<any>>()
@@ -11,30 +10,6 @@ jest.mock('../src/parts/InitializeExtensionHostWorker/InitializeExtensionHostWor
 
 beforeEach(() => {
   mockCreateExtensionHostRpc.mockReset()
-})
-
-test.skip('invoke should call rpc.invoke with method and params', async () => {
-  let invokeCallCount = 0
-  let lastMethod: string | undefined
-  let lastParams: readonly unknown[] | undefined
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: async (method: string, ...params: readonly unknown[]) => {
-      invokeCallCount++
-      lastMethod = method
-      lastParams = params
-      return undefined
-    },
-  })
-  ExtensionHost.set(mockRpc)
-
-  await ExtensionHostWorker.invoke('testMethod', 'param1', 'param2')
-
-  expect(invokeCallCount).toBe(1)
-  expect(lastMethod).toBe('testMethod')
-  expect(lastParams).toEqual(['param1', 'param2'])
-
-  await mockRpc.dispose()
 })
 
 test.skip('invoke should call rpc.invoke with method and params', async () => {
