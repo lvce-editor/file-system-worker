afdef4b0a8dcfc0646b91f4a8fd89ffa48040fa1
Le Vivilet <levivilet@pm.me>
2025-12-04 19:09:49 +0100

    feature: use lazy rpc for renderer process (#122)

--- packages/file-system-worker/test/UploadFileSystemHandle.test.ts
+++ packages/file-system-worker/test/UploadFileSystemHandle.test.ts
@@ -1,8 +1,59 @@
+/* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
 import { beforeEach, expect, jest, test } from '@jest/globals'
 import { MockRpc } from '@lvce-editor/rpc'
 import * as FileSystemProcess from '../src/parts/FileSystemProcess/FileSystemProcess.ts'
-import * as RendererProcess from '../src/parts/RendererProcess/RendererProcess.ts'
+import { setFactory } from '../src/parts/RendererProcess/RendererProcess.ts'
 import * as UploadFileSystemHandle from '../src/parts/UploadFileSystemHandle/UploadFileSystemHandle.ts'
+
+// @ts-ignore
+globalThis.ProgressEvent = class ProgressEvent extends Event {
+  constructor(type: string, init?: any) {
+    super(type, init)
+    this.target = init?.target
+  }
+  target: any
+}
+
+// @ts-ignore
+globalThis.FileReader = class FileReader extends EventTarget {
+  result: string | ArrayBuffer | null = null
+  readyState: number = 0
+  error: Error | null = null
+  onload: ((event: any) => void) | null = null
+  onerror: ((event: any) => void) | null = null
+  onloadend: ((event: any) => void) | null = null
+
+  readAsBinaryString(blob: Blob): void {
+    this.readyState = 1
+    blob
+      .arrayBuffer()
+      .then((buffer) => {
+        const bytes = new Uint8Array(buffer)
+        let binaryString = ''
+        for (let i = 0; i < bytes.length; i++) {
+          binaryString += String.fromCodePoint(bytes[i])
+        }
+        this.result = binaryString
+        this.readyState = 2
+        if (this.onload) {
+          this.onload({ target: this })
+        }
+        if (this.onloadend) {
+          this.onloadend({ target: this })
+        }
+      })
+      .catch((error) => {
+        this.error = error
+        this.readyState = 2
+        if (this.onerror) {
+          this.onerror({ target: this })
+        }
+        if (this.onloadend) {
+          this.onloadend({ target: this })
+        }
+      })
+  }
+}
 
 const mockFileSystemInvoke = jest.fn<(method: string, ...args: readonly unknown[]) => Promise<unknown>>()
 const mockFileSystemRpc = MockRpc.create({
@@ -19,7 +70,7 @@ const mockRendererRpc = MockRpc.create({
 beforeEach(() => {
   jest.resetAllMocks()
   FileSystemProcess.set(mockFileSystemRpc)
-  RendererProcess.set(mockRendererRpc)
+  setFactory(async () => mockRendererRpc)
 })
 
 test('uploadHandle with file', async () => {
