c9923fc28c4ab0e41dbf869be1614e769fb7d6f8
Le Vivilet <levivilet@pm.me>
2025-12-04 17:45:46 +0100

    feature: use lazy rpc for extension host worker (#120)

--- packages/file-system-worker/src/parts/Listen/Listen.ts
+++ packages/file-system-worker/src/parts/Listen/Listen.ts
@@ -1,10 +1,13 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
+import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
+import { createExtensionHostRpc } from '../InitializeExtensionHostWorker/InitializeExtensionHostWorker.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const listen = async (): Promise<void> => {
   Object.assign(CommandMapRef.commandMapRef, CommandMap.commandMap)
+  ExtensionHostWorker.setFactory(createExtensionHostRpc)
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
