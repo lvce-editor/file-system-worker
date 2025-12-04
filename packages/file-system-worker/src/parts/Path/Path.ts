b8e46793ba87cf96c6075ef203eafd57ad049af3
Le Vivilet <levivilet@pm.me>
2025-12-04 17:04:04 +0100

    lint

--- packages/file-system-worker/src/parts/Path/Path.ts
+++ packages/file-system-worker/src/parts/Path/Path.ts
@@ -1,6 +1,3 @@
-import * as Assert from '../Assert/Assert.ts'
-import * as GetFileExtension from '../GetFileExtension/GetFileExtension.ts'
-
 export const join = (pathSeparator: string, ...parts: readonly string[]): string => {
   return parts.join(pathSeparator)
 }
