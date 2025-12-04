c79e3a4970e8fca96a3b56de170394b754af9398
Le Vivilet <levivilet@pm.me>
2025-12-04 17:44:10 +0100

    feature: use lazy rpc for extension host worker

--- packages/file-system-worker/src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
+++ packages/file-system-worker/src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
@@ -1,20 +1,3 @@
-import type { Rpc } from '@lvce-editor/rpc-registry'
-import { ExtensionHost } from '@lvce-editor/rpc-registry'
-import { createExtensionHostRpc } from '../InitializeExtensionHostWorker/InitializeExtensionHostWorker.ts'
+import { createLazyRpc, RpcId } from '@lvce-editor/rpc-registry'
 
-export const { set } = ExtensionHost
-
-let rpcPromise: Promise<Rpc> | undefined = undefined
-
-const getOrCreate = (): Promise<Rpc> => {
-  if (!rpcPromise) {
-    rpcPromise = createExtensionHostRpc()
-  }
-
-  return rpcPromise
-}
-
-export const invoke = async (method: string, ...params: readonly any[]): Promise<any> => {
-  const rpc = await getOrCreate()
-  return rpc.invoke(method, ...params)
-}
+export const { invoke, setFactory } = createLazyRpc(RpcId.ExtensionHostWorker)
