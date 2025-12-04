99b54b263f97626c2139951caa80d9a3cffac2a7
Le Vivilet <levivilet@pm.me>
2025-12-04 19:44:02 +0100

    feature: setup renderer process rpc

--- packages/file-system-worker/src/parts/Listen/Listen.ts
+++ packages/file-system-worker/src/parts/Listen/Listen.ts
@@ -3,11 +3,14 @@ import * as CommandMap from '../CommandMap/CommandMap.ts'
 import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
 import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
 import { createExtensionHostRpc } from '../InitializeExtensionHostWorker/InitializeExtensionHostWorker.ts'
+import { createRendererProcessRpc } from '../InitializeRendererProcess/InitializeRendererProcess.ts'
+import * as RendererProcess from '../RendererProcess/RendererProcess.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const listen = async (): Promise<void> => {
   Object.assign(CommandMapRef.commandMapRef, CommandMap.commandMap)
   ExtensionHostWorker.setFactory(createExtensionHostRpc)
+  RendererProcess.setFactory(createRendererProcessRpc)
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
