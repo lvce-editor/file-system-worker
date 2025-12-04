a2929dde91c7c2f871b5bf778f6e19f3276ba111
Le Vivilet <levivilet@pm.me>
2025-12-04 18:53:07 +0100

    import

--- packages/file-system-worker/test/UploadFileSystemHandles.test.ts
+++ packages/file-system-worker/test/UploadFileSystemHandles.test.ts
@@ -1,15 +1,10 @@
-import { expect, jest, test, beforeEach } from '@jest/globals'
+import { expect, jest, test } from '@jest/globals'
 import { MockRpc } from '@lvce-editor/rpc'
 import { RendererProcess } from '@lvce-editor/rpc-registry'
 import { setFactory } from '../src/parts/RendererProcess/RendererProcess.ts'
+import * as UploadFileSystemHandles from '../src/parts/UploadFileSystemHandles/UploadFileSystemHandles.ts'
 
-beforeEach(() => {
-  jest.resetModules()
-})
-
-test('uploadFileSystemHandles with single directory', async () => {
-  const UploadFileSystemHandles = await import('../src/parts/UploadFileSystemHandles/UploadFileSystemHandles.ts')
-
+test.only('uploadFileSystemHandles with single directory', async () => {
   const mockRendererInvoke = jest.fn<(method: string, ...args: readonly unknown[]) => Promise<unknown>>()
   const rpc = MockRpc.create({
     commandMap: {},
