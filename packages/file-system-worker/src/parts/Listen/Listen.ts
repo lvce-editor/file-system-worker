2844b07b80743d9a70a7210652fb803a7111636b
Le Vivilet <levivilet@pm.me>
2026-01-13 15:15:39 +0100

    feature: move intializeRendererWorker into separate file

--- packages/file-system-worker/src/parts/Listen/Listen.ts
+++ packages/file-system-worker/src/parts/Listen/Listen.ts
@@ -1,18 +1,14 @@
-import { WebWorkerRpcClient } from '@lvce-editor/rpc'
-import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
 import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
 import { createExtensionHostRpc } from '../InitializeExtensionHostWorker/InitializeExtensionHostWorker.ts'
 import { createRendererProcessRpc } from '../InitializeRendererProcess/InitializeRendererProcess.ts'
+import { initializeRendererWorker } from '../InitializeRendererWorker/InitializeRendererWorker.ts'
 import * as RendererProcess from '../RendererProcess/RendererProcess.ts'
 
 export const listen = async (): Promise<void> => {
   Object.assign(CommandMapRef.commandMapRef, CommandMap.commandMap)
   ExtensionHostWorker.setFactory(createExtensionHostRpc)
   RendererProcess.setFactory(createRendererProcessRpc)
-  const rpc = await WebWorkerRpcClient.create({
-    commandMap: CommandMap.commandMap,
-  })
-  RendererWorker.set(rpc)
+  await initializeRendererWorker()
 }
