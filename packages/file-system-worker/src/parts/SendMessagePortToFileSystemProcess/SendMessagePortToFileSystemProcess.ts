82aff5b744faabac09caac105318fdde83139503
Le Vivilet <levivilet@pm.me>
2026-04-16 13:27:26 +0200

    refactor: simplify sendMessagePortToFileSystemProcess implementation

--- packages/file-system-worker/src/parts/SendMessagePortToFileSystemProcess/SendMessagePortToFileSystemProcess.ts
+++ packages/file-system-worker/src/parts/SendMessagePortToFileSystemProcess/SendMessagePortToFileSystemProcess.ts
@@ -1,6 +1,5 @@
 import { RendererWorker, RpcId } from '@lvce-editor/rpc-registry'
 
 export const sendMessagePortToFileSystemProcess = async (port: MessagePort): Promise<void> => {
-  const command = 'HandleMessagePortForFileSystemProcess.handleMessagePortForFileSystemProcess'
-  await RendererWorker.invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToSharedProcess', port, command, RpcId.FileSystemWorker)
+  await RendererWorker.sendMessagePortToFileSystemProcess(port, RpcId.FileSystemWorker)
 }
