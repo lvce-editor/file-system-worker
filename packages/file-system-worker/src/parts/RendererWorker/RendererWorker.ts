8efeb8622aa213f40dd5b9e007aebedf320be603
Le Vivilet <levivilet@pm.me>
2026-07-30 16:19:41 +0200

    feature: add knip (#171)

--- packages/file-system-worker/src/parts/RendererWorker/RendererWorker.ts
+++ packages/file-system-worker/src/parts/RendererWorker/RendererWorker.ts
@@ -1,3 +1,3 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
-export const { invokeAndTransfer, sendMessagePortToExtensionHostWorker, set } = RendererWorker
+export const { set } = RendererWorker
--- packages/file-system-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts
+++
@@ -1,5 +0,0 @@
-import { RendererWorker, RpcId } from '@lvce-editor/rpc-registry'
-
-export const sendMessagePortToFileSystemProcess = async (port: MessagePort): Promise<void> => {
-  await RendererWorker.sendMessagePortToExtensionHostWorker(port, RpcId.FileSystemWorker)
-}
